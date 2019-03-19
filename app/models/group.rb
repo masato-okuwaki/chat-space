cf800a5e468d6290e506483e3670154130fa7117
masato-okuwaki <masato.okuwaki@di-v.co.jp>
2019-03-19 14:38:46 +0900

    commit_login_view
    ### WHAT
    deviseの設定。Flashの設定。
    
    ### WHY
    ユーザーのログイン機能の実装のため

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
  has_many :messages
  has_many :users, through: :users_groups
  has_many :users_groups
+ validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
 
   has_many :messages
   has_many :groups, through: :users_groups
   has_many :users_groups
-
 end
